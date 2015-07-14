529768f261622e71782ef1953bf471df14cfdbb2
suliutree <413474315@qq.com>
2015-07-14 10:05:39 +0800

    Create 1-0.cpp

---
+++ 1-0.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cout << "please enter your name: " << endl;
+	string name;
+	cin >> name;
+	cout << "hello, " << name << endl;
+	system("pause");
+	return 0;
+}
