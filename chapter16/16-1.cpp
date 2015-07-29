c9294d7afc756a0579172bc4b3c3699a30b0ac35
suliutree <413474315@qq.com>
2015-07-29 22:26:39 +0800

    commit

---
+++ chapter16/16-1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	ifstream infile("16-1.cpp");
+
+	string s;
+	while (getline(infile, s))
+	{
+		cout << s << endl;
+	}
+
+	system("pause");
+	return 0;
+}
